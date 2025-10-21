f23ae5f56af8a76fd8682dd8a63d371987334b33
João Santos <ricardoelric@gmail.com>
2025-10-21 08:06:55 -0300

    wip: first version of the navigation bar to implement test features

--- app/src/main/java/com/joaosant0s/pockettools/MainActivity.kt
+++ app/src/main/java/com/joaosant0s/pockettools/MainActivity.kt
@@ -4,17 +4,25 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import com.joaosant0s.pockettools.core.floating.FloatingController
 
 import com.joaosant0s.pockettools.tools.ToolsBuilder
-import com.joaosant0s.pockettools.tools.collection.ToolLantern
+import com.joaosant0s.pockettools.utils.Message
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolsBuilder: ToolsBuilder
     private lateinit var floatingController : FloatingController
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var topAppBar: MaterialToolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +35,39 @@ class MainActivity : AppCompatActivity() {
         toolsBuilder = ToolsBuilder(this)
         floatingController = FloatingController(this)
 
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        topAppBar = findViewById(R.id.topAppBar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        drawerLayout.setDrawerLockMode(
+            DrawerLayout.LOCK_MODE_LOCKED_CLOSED,
+            GravityCompat.START
+        )
+
+        // Hamburger icon opens drawer
+        topAppBar.setNavigationOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                drawerLayout.closeDrawer(GravityCompat.END)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.END)
+            }
+        }
+
+        // Handle menu item clicks
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> showToast("Home selected")
+                R.id.nav_settings -> showToast("Settings selected")
+                R.id.nav_help -> showToast("Help selected")
+            }
+//            drawerLayout.closeDrawer(GravityCompat.END)
+            true
         }
 
         toolsBuilder = toolsBuilder.addLockScreen().addVolume()
@@ -50,5 +87,18 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
 
         floatingController.tryStart();
+    }
+
+    private fun showToast(message: String) {
+        Message.showToast(this, message)
+    }
+
+    override fun onBackPressed() {
+        // Close drawer first if open
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
